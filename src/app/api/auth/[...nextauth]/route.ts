436198346958bef506f71047a46b795cba83292e
motorcykle <mohamedabdulridha1@gmail.com>
2024-01-08 00:59:05 +0100

    db + some routes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,13 @@
 
-import { prisma } from "@/lib/db";
+import { PrismaClient } from '@prisma/client'
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { AuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
+const prisma = new PrismaClient()
+
 export const authOptions: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma) as any,
   providers: [
     GoogleProvider({
@@ -12,6 +15,11 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     })
   ],
+  events: {
+    async signIn(message) {
+      console.log(message.isNewUser)
+    }
+  }
 }
 
 const handler = NextAuth(authOptions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client"
-import { signIn, signOut } from 'next-auth/react'
+import { getSession, signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 
 export default function Home() {
+  const session = useSession()
+
   return (
     <main className="">
+      <p>{session?.data?.user?.name}</p>
       <button onClick={() => signIn()}>Log in</button>
       <button onClick={() => signOut()}>Log out</button>
     </main>
