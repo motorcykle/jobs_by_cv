b5c399761213c7403c5ef62a5555b90d5b1646bf
motorcykle <mohamedabdulridha1@gmail.com>
2024-01-13 18:25:49 +0100

    middleware change test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,17 @@
-import { withAuth } from "next-auth/middleware"
+// import { withAuth } from "next-auth/middleware"
 
-export default withAuth({
-  callbacks: {
-    authorized: ({ req }) => {
-      const sessionToken = req.cookies.get("next-auth.session-token");
-      if (sessionToken) return true;
-      else return false;
-    },
-  },
-});
+// export default withAuth({
+//   callbacks: {
+//     authorized: ({ req }) => {
+//       const sessionToken = req.cookies.get("next-auth.session-token");
+//       if (sessionToken) return true;
+//       else return false;
+//     },
+//   },
+// });
 
-export const config = { matcher: ["/listings", "/listings/:path*"] }
+// export const config = { matcher: ["/listings", "/listings/:path*"] }
+
+export { default } from "next-auth/middleware"
+
+export const config = { matcher: ["/listings"] }
